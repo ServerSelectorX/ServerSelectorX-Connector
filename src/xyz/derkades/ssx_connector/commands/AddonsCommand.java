e7835c3e52c190d60aa312216cf53b18a635684b
Robin <robinslot@hotmail.nl>
2019-12-15 13:46:46 +0100

    Implement addons command

--- src/xyz/derkades/ssx_connector/commands/AddonsCommand.java
+++ src/xyz/derkades/ssx_connector/commands/AddonsCommand.java
@@ -1,5 +1,6 @@
 package xyz.derkades.ssx_connector.commands;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,45 +19,45 @@ public class AddonsCommand implements CommandCallable {
 	@Override
 	public CommandResult process(final CommandSource source, final String arguments) throws CommandException {
 		if (Main.instance.addons.isEmpty()) {
-			sender.sendMessage("There are no addons installed. The placeholders {online} and {max} are always available.");
-			return true;
+			source.sendMessage(Text.of("There are no addons installed. The placeholders {online} and {max} are always available."));
+			return CommandResult.success();
 		}
 
-		sender.sendMessage("The following addons are installed:");
+		source.sendMessage(Text.of("The following addons are installed:"));
 
-		Main.instance.addons.forEach((addon) -> sender.sendMessage(String.format(
+		Main.instance.addons.forEach((addon) -> source.sendMessage(Text.of(String.format(
 				"%s by %s version %s license %s: '%s'",
 				addon.getName(), addon.getAuthor(), addon.getVersion(),
-				addon.getLicense(), addon.getDescription())));
+				addon.getLicense(), addon.getDescription()))));
 
-		sender.sendMessage("To view placeholders for each addon, use the command /ssxc placeholders <addon>.");
-		return true;
+		source.sendMessage(Text.of("To view placeholders for each addon, use the command /ssxc placeholders <addon>."));
+		return CommandResult.success();
 	}
 
 	@Override
 	public List<String> getSuggestions(final CommandSource source, final String arguments, final Location<World> targetPosition)
 			throws CommandException {
-		return null;
+		return Arrays.asList();
 	}
 
 	@Override
 	public boolean testPermission(final CommandSource source) {
-		return false;
+		return source.hasPermission("ssxc.addons");
 	}
 
 	@Override
 	public Optional<Text> getShortDescription(final CommandSource source) {
-		return null;
+		return Optional.of(Text.of("List installed addons"));
 	}
 
 	@Override
 	public Optional<Text> getHelp(final CommandSource source) {
-		return null;
+		return Optional.empty();
 	}
 
 	@Override
 	public Text getUsage(final CommandSource source) {
-		return null;
+		return Text.of("/ssxc addons");
 	}
 
 }
