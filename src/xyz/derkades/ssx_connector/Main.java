a5d218243495075e9aa53902cee1de17eec55e18
Derkades <robinslot@hotmail.nl>
2020-09-03 19:20:47 +0200

    Operator precedence issue maybe?

--- src/xyz/derkades/ssx_connector/Main.java
+++ src/xyz/derkades/ssx_connector/Main.java
@@ -129,7 +129,7 @@ public class Main extends JavaPlugin {
 
 		final int addresses = getConfig().getStringList("addresses").size();
 		final int interval = getConfig().getInt("send-interval");
-		final int taskIntervalTicks = interval * 20 / addresses;
+		final int taskIntervalTicks = (interval * 20) / addresses;
 
 		this.pingTask = Bukkit.getScheduler().runTaskTimerAsynchronously(this, new PlaceholderSender(), 40, taskIntervalTicks);
 	}
