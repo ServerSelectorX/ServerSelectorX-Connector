5b972122516e8dc929cc3a8fe5bdfb77ff8e0e1d
Robin Slot <robin@rslot.nl>
2025-11-03 16:47:48 +0100

    fix bugs in folia scheduler

--- src/xyz/derkades/ssx_connector/scheduler/FoliaSchedulerWrapper.java
+++ src/xyz/derkades/ssx_connector/scheduler/FoliaSchedulerWrapper.java
@@ -2,6 +2,7 @@ package xyz.derkades.ssx_connector.scheduler;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.function.Consumer;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Server;
@@ -20,9 +21,9 @@ public class FoliaSchedulerWrapper implements SchedulerWrapper {
             this.plugin = plugin;
             final Server server = Bukkit.getServer();
             this.asyncScheduler = server.getClass().getMethod("getAsyncScheduler").invoke(server);
-            this.asyncRunNowMethod = this.asyncScheduler.getClass().getMethod("runNow", Plugin.class, Runnable.class);
+            this.asyncRunNowMethod = this.asyncScheduler.getClass().getMethod("runNow", Plugin.class, Consumer.class);
             this.globalRegionScheduler = server.getClass().getMethod("getGlobalRegionScheduler").invoke(server);
-            this.runTimerMethod = this.globalRegionScheduler.getClass().getMethod("runAtFixedRate", Plugin.class, Runnable.class, int.class, int.class);
+            this.runTimerMethod = this.globalRegionScheduler.getClass().getMethod("runAtFixedRate", Plugin.class, Consumer.class, long.class, long.class);
         } catch (final InvocationTargetException | IllegalAccessException | NoSuchMethodException | SecurityException e) {
             throw new RuntimeException(e);
         }
@@ -31,16 +32,19 @@ public class FoliaSchedulerWrapper implements SchedulerWrapper {
     @Override
     public FoliaScheduledTask runAsync(final Runnable runnable) {
         try {
-            return new FoliaScheduledTask(this.asyncRunNowMethod.invoke(this.asyncScheduler, this.plugin, runnable));
+            final Consumer<?> task = task2 -> runnable.run();
+            return new FoliaScheduledTask(this.asyncRunNowMethod.invoke(this.asyncScheduler, this.plugin, task));
         } catch (IllegalAccessException | InvocationTargetException e) {
             throw new RuntimeException(e);
         }
     }
 
     @Override
-    public FoliaScheduledTask runTimer(final Runnable runnable, final int delay, final int interval) {
+    public FoliaScheduledTask runTimer(final Runnable runnable, int delay, final int interval) {
+        delay = delay > 0 ? delay : 1; // folia cannot schedule task with no delay
         try {
-            return new FoliaScheduledTask(this.runTimerMethod.invoke(this.globalRegionScheduler, this.plugin, runnable, delay, interval));
+            final Consumer<?> task = task2 -> runnable.run();
+            return new FoliaScheduledTask(this.runTimerMethod.invoke(this.globalRegionScheduler, this.plugin, task, delay, interval));
         } catch (IllegalAccessException | InvocationTargetException e) {
             throw new RuntimeException(e);
         }
